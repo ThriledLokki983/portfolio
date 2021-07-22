02bc0d8daba6e3011eb4d7f9f5f95dd7e725d692
ThriledLokki983 <gnimoh001@gmail.com>
2021-07-22 21:17:26 +0200

    implemented the header with the nav-bars

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 window.addEventListener("DOMContentLoaded", () => {
     const anim = anime.timeline({
-        easing: "easeOutBack",
+        easing: "easeOutExpo",
         duration: 750,
     });
 
@@ -8,14 +8,23 @@ window.addEventListener("DOMContentLoaded", () => {
         targets: "#hexagon path",
         strokeDashoffset: [anime.setDashoffset, 0],
         easing: "easeInOutQuart",
-        duration: 2000,
+        baseFrequency: 0,
+        opacity: 1,
+        duration: 1000,
         delay: function (el, i) {
             return i * 250;
         },
     }).add({
         targets: "#hexagon #G",
-        duration: 1000,
+        baseFrequency: 0,
+        duration: 1500,
         opacity: 1,
         easing: "easeInOutQuart",
     });
 });
+
+const logo = document.querySelector(".intro__container");
+
+setTimeout(() => {
+    logo.classList.add("hidden");
+}, 4000);
