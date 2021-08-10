8c6125602e20a22d167b098b6582675ae10b166e
ThriledLokki983 <gnimoh001@gmail.com>
2021-08-10 19:30:37 +0200

    added the experience data

--- js/data.js
+++ js/data.js
@@ -1,77 +1,100 @@
+/** @format */
+
 const projects = [
-    {
-        id: 1,
-        name: "Mapty App",
-        label: "Featured Project",
-        description:
-            "Search for over 1million+ of recipes for an inspiration of something to cook or\
+	{
+		id: 1,
+		name: "Mapty App",
+		label: "Featured Project",
+		desc: "Search for over 1million+ of recipes for an inspiration of something to cook or\
             create the perfect recipe and save it online so you never forget. With a simple API\
             query, this app will spit out all recipes related to the search word you provide",
-        langueages: ["Javascript", "GitHub API", "Sass"],
-        photo: "recipe.jpg",
-        links: ["github", "external-link"],
-    },
-    {
-        id: 2,
-        name: "Pig Game",
-        label: "Featured Project",
-        description:
-            "In this game, two players will have to compete to reach 100pts by\
+		languages: ["Javascript", "GitHub API", "Sass"],
+		photo: "recipe.jpg",
+		links: ["github", "external-link"],
+	},
+	{
+		id: 2,
+		name: "Pig Game",
+		label: "Featured Project",
+		desc: "In this game, two players will have to compete to reach 100pts by\
             rolling a dice. Wait! if you roll a 1, all your accumulated value goes down to 0.\
             The strategy is to use the hold button to save any accumulated number. When a value\
-            is held, it adds to your overall total points and rolling a 1 willl not affect that.",
-        langueages: ["Javascript", "CSS3"],
-        photo: "pig-game.jpg",
-        links: ["github", "external-link"],
-    },
-    {
-        id: 3,
-        name: "Mapty",
-        label: "Featured Project",
-        description:
-            "Looking for a personalized training web-app? Check out Mapty, you can\
+            is held, it adds to your overall total points and rolling a 1 will not affect that.",
+		languages: ["Javascript", "CSS3"],
+		photo: "pig-game.jpg",
+		links: ["github", "external-link"],
+	},
+	{
+		id: 3,
+		name: "Mapty",
+		label: "Featured Project",
+		desc: "Looking for a personalized training web-app? Check out Mapty, you can\
             record on the map the exact location you would want to cycle or go for running. \
             This simple web-app will keep track of all the activities in one place for your ease of use!",
-        langueages: ["Javascript", "GitHub API", "Sass"],
-        photo: "mapty.jpg",
-        links: ["github", "external-link"],
-    },
-    {
-        id: 4,
-        name: "Bankist",
-        label: "Featured Project",
-        description:
-            " A very modern looking website with scroll-into-view and tons of\
+		languages: ["Javascript", "GitHub API", "Sass"],
+		photo: "mapty.jpg",
+		links: ["github", "external-link"],
+	},
+	{
+		id: 4,
+		name: "Bankist",
+		label: "Featured Project",
+		desc: " A very modern looking website with scroll-into-view and tons of\
              animations developed with only CSS and JavaScript.",
-        langueages: ["Javascript", "CSS3"],
-        photo: "bankist.jpg",
-        links: ["github", "external-link"],
-    },
+		languages: ["Javascript", "CSS3"],
+		photo: "bankist.jpg",
+		links: ["github", "external-link"],
+	},
 ];
 
-const enxperience = [
-    {
-        id: 1,
-        name: "front",
-        desc: "I value simple but well thought-through web content structure,\
-        which indeed has clean design patterns, and also interractive for the users.",
-        icon: "browser",
-        enjoy: [],
-        tools: [],
-    },
+const experience = [
+	{
+		id: 1,
+		name: "front",
+		desc: "I value simple but well thought-through web content structure,\
+        which indeed has clean design patterns, and also interactive for the users.",
+		icon: "browser",
+		enjoyTitle: "What I enjoy designing:",
+		enjoy: ["Mobile", "Web", "App"],
+		toolsTitle: "Tools worked with",
+		tools: ["React", "Pug", "CodePen", "WordPress", "Javascript", "XD/Figma"],
+	},
 
-    {
-        id: 2,
-        name: "back",
-        desc: "",
-        enjoy: [],
-        tools: [],
-    },
-    {
-        id: 3,
-        name: "software",
-        desc: "",
-        enjoy: [],
-        tools: [],
-    },
+	{
+		id: 2,
+		name: "back",
+		desc: "",
+		icon: "code",
+		enjoyTitle: "What I enjoy building:",
+		enjoy: ["Web Apps", "Websites"],
+		toolsTitle: "Tools worked with",
+		tools: ["Express", "Nodejs", "Django", "Flask"],
+	},
+
+	{
+		id: 3,
+		name: "software",
+		desc: "I have worked as an Application support Analyst with ASUS Tech for\
+        almost 2-years, studied all the systems developed and suggested\
+        improvements to these systems. Based on request by our Global users, I\
+        helped to clarify and come up with Design and Specification for the\
+        developers to implement. here, I have gunned a lot of experiences\
+        of what users expect with their request for systems they make use of everyday.",
+		icon: "document-code",
+		enjoyTitle: "Where I have Worked:",
+		enjoy: [
+			" ASUS Tech. (2-yrs)",
+			"Uni. of Groningen (6-months)",
+			"Uni. of Ghana (1-yr)",
+		],
+		toolsTitle: "Tools worked with",
+		tools: [
+			"MySQL",
+			"phpStorm",
+			"phpMyAdmin",
+			"MSSQL Server",
+			"WordPress",
+			"RStudio",
+		],
+	},
 ];
--- js/script.js
+++ js/script.js
@@ -1,103 +1,99 @@
+/** @format */
+
 "use-strict";
 window.addEventListener("DOMContentLoaded", (event) => {
-    // const docWidth = document.documentElement.offsetWidth;
+	// const docWidth = document.documentElement.offsetWidth;
 
-    // [].forEach.call(document.querySelectorAll("*"), function (el) {
-    //     if (el.offsetWidth > docWidth) {
-    //         console.log(el);
-    //     }
-    // });
+	// [].forEach.call(document.querySelectorAll("*"), function (el) {
+	//     if (el.offsetWidth > docWidth) {
+	//         console.log(el);
+	//     }
+	// });
 
-    // const nav = document.querySelector(".navigation");
-    const mobile_logo = document.querySelector(".header__nav");
-    const mainContent = document.querySelector(".content");
-    const modal = document.querySelector(".modal");
-    const modalNav = document.querySelector(".modal__nav");
-    const home = document.querySelector("#home");
-    const logoBox = document.querySelector(".header__logo-box");
+	// const nav = document.querySelector(".navigation");
+	const mobile_logo = document.querySelector(".header__nav");
+	const mainContent = document.querySelector(".content");
+	const modal = document.querySelector(".modal");
+	const modalNav = document.querySelector(".modal__nav");
+	const home = document.querySelector("#home");
+	const logoBox = document.querySelector(".header__logo-box");
 
-    const tabs = document.querySelectorAll(".operations__tab");
-    const tabsContainer = document.querySelector(".operations__tab-container");
-    const tabsContent = document.querySelectorAll(".operations__content");
+	const tabs = document.querySelectorAll(".operations__tab");
+	const tabsContainer = document.querySelector(".operations__tab-container");
+	const tabsContent = document.querySelectorAll(".operations__content");
 
-    const mobileMenu = function () {
-        mainContent.classList.toggle("hidden");
-        modal.style.display = "flex";
-        modal.classList.toggle("hidden");
-        modalNav.classList.toggle("hidden");
-    };
+	const mobileMenu = function () {
+		mainContent.classList.toggle("hidden");
+		modal.style.display = "flex";
+		modal.classList.toggle("hidden");
+		modalNav.classList.toggle("hidden");
+	};
 
-    const viewSection = function (e) {
-        const id = e.target.getAttribute("href");
-        document.querySelector(id).scrollIntoView({
-            behavior: "smooth",
-        });
-    };
+	const viewSection = function (e) {
+		const id = e.target.getAttribute("href");
+		document.querySelector(id).scrollIntoView({
+			behavior: "smooth",
+		});
+	};
 
-    logoBox.addEventListener("click", function (e) {
-        if (e.target.classList.contains("header__logo-box--text")) {
-            home.scrollIntoView({
-                behavior: "smooth",
-            });
-        }
-    });
+	logoBox.addEventListener("click", function (e) {
+		if (e.target.classList.contains("header__logo-box--text")) {
+			home.scrollIntoView({
+				behavior: "smooth",
+			});
+		}
+	});
 
-    mobile_logo.addEventListener("click", function (e) {
-        if (e.target.classList.contains("header__nav")) {
-            mobileMenu();
-        }
-    });
+	mobile_logo.addEventListener("click", function (e) {
+		if (e.target.classList.contains("header__nav")) {
+			mobileMenu();
+		}
+	});
 
-    document
-        .querySelector(".header__nav--list")
-        .addEventListener("click", function (e) {
-            e.preventDefault();
+	document.querySelector(".header__nav--list").addEventListener("click", function (e) {
+		e.preventDefault();
 
-            if (e.target.classList.contains("header__nav--link")) {
-                viewSection(e);
-            }
-        });
+		if (e.target.classList.contains("header__nav--link")) {
+			viewSection(e);
+		}
+	});
 
-    document
-        .querySelector(".modal__nav--list")
-        .addEventListener("click", function (e) {
-            e.preventDefault();
+	document.querySelector(".modal__nav--list").addEventListener("click", function (e) {
+		e.preventDefault();
 
-            mobileMenu();
+		mobileMenu();
 
-            if (e.target.classList.contains("modal__nav--link")) {
-                viewSection(e);
-            } else {
-                home.scrollIntoView({
-                    behavior: "smooth",
-                });
-            }
-        });
+		if (e.target.classList.contains("modal__nav--link")) {
+			viewSection(e);
+		} else {
+			home.scrollIntoView({
+				behavior: "smooth",
+			});
+		}
+	});
 
-    //Control the enxperience buttons and its content
-    tabsContainer.addEventListener("click", function (e) {
-        const clicked = e.target.closest(".operations__tab");
-        console.log(clicked);
+	//Control the experience buttons and its content
+	tabsContainer.addEventListener("click", function (e) {
+		const clicked = e.target.closest(".operations__tab");
+		console.log(clicked);
 
-        if (!clicked) return;
+		if (!clicked) return;
 
-        tabs.forEach((t) => t.classList.remove("operations__tab--active"));
-        clicked.classList.toggle("btn-1");
+		tabs.forEach((t) => t.classList.remove("operations__tab--active"));
+		clicked.classList.toggle("btn-1");
 
-        tabsContent.forEach((c) =>
-            c.classList.remove("operations__content--active")
-        );
+		tabsContent.forEach((c) => c.classList.remove("operations__content--active"));
 
-        tabs.forEach((tab) => tab.classList.remove("btn-1"));
+		tabs.forEach((tab) => tab.classList.remove("btn-1"));
 
-        document
-            .querySelector(`.operations__content--${clicked.dataset.tab}`)
-            .classList.add("operations__content--active");
+		document
+			.querySelector(`.operations__content--${clicked.dataset.tab}`)
+			.classList.add("operations__content--active");
 
-        document
-            .querySelector(`.operations__tab--${clicked.dataset.tab}`)
-            .classList.add("btn-1");
-    });
+		document
+			.querySelector(`.operations__tab--${clicked.dataset.tab}`)
+			.classList.add("btn-1");
+	});
 });
 
 // const docWidth = document.documentElement.offsetWidth;
